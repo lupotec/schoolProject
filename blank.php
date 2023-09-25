2510504a6bf590460f8480713bfc79f59b2e214a
lupotec <138595746+lupotec@users.noreply.github.com>
2023-09-25 14:47:58 +0200

    Update blank.php

--- blank.php
+++ blank.php
@@ -54,12 +54,7 @@
 				<div class="col-xs-2"><b>YouTube</b></div>
 			</div>
 			
-			<!--   SELECT LIJST   ->
-			<div class='row' STYLE='border:1px solid blue; b color:#337ab7;'>
-				<div class="col-xs-5"><b></b></div>
-				<div class="col-xs-5"><b></b></div>
-				<div class="col-xs-2"><b></b></div>
-			</div>		
+			
 			
 			
 		</div>
