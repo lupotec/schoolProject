d99801b6a95f36c55ccc1e60abb44032e8ea5567
lupotec <138595746+lupotec@users.noreply.github.com>
2023-09-25 16:15:24 +0200

    Delete list.php

--- list.php
+++
@@ -1,6 +0,0 @@
-<!--   SELECT LIJST   ->
-			<div class='row' STYLE='border:1px solid blue; b color:#337ab7;'>
-				<div class="col-xs-5"><b></b></div>
-				<div class="col-xs-5"><b></b></div>
-				<div class="col-xs-2"><b></b></div>
-			</div>		
